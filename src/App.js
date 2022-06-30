c869f8897fc11bf01f2c29a711d85bfdc09305e9
Deadrok-z <84705521+Deadrok-z@users.noreply.github.com>
2022-06-30 16:19:12 +0300

    refactoring and return selectQuestion

--- src/App.js
+++ src/App.js
@@ -1,56 +1,60 @@
 import React, { useState } from "react";
-import {questions} from './questions';
+import { questions } from './questions';
 import Question from './Question/Question';
 import QuestionButtons from './QuestionButtons/QuestionButtons';
 import './App.css';
 
 export default function App() {
-  
+
+  const arr = questions.map((elem, index) => {
+    return { index, value: "" }
+  })
   const [score, setScore] = useState(0);
   const [questionIndex, setQuestionIndex] = useState(0);
   const [answer, setAnswer] = useState("");
+  const [answers, setAnswers] = useState([...arr]);
   const restart = () => {
     setScore(0);
     setAnswer("");
     setQuestionIndex(0);
+    setAnswers([...arr]);
   };
   const submit = (e) => {
     e.preventDefault();
+    const temp = [...answers];
     if (answer === questions[questionIndex].rightAnswer) {
       setScore((score) => score + 1);
-      document.getElementsByClassName('element')[questionIndex].style = "background: green";
+      temp[questionIndex].value = true
+      setAnswers([...temp]);
     } else {
-      document.getElementsByClassName('element')[questionIndex].style = "background: red";
+      temp[questionIndex].value = false
+      setAnswers([...temp]);
     }
     if (questionIndex < questions.length) {
       setQuestionIndex((i) => i + 1);
     }
   };
-if (questionIndex < questions.length) {
-    return (
-      <div>
-        <h1 className='d-flex justify-content-center'>Викторина</h1>
-        <QuestionButtons questions={[...questions]} setQuestionIndex={setQuestionIndex}/>
-        <Question arrProps={[questions,questionIndex,setAnswer,answer,submit]} />
-        <p className='d-flex justify-content-center border border-dark py-2 bg-white'>счет: {score} / {questions.length}</p>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1 className='d-flex justify-content-center'>Викторина</h1>
-        <QuestionButtons questions={[...questions]} setQuestionIndex={setQuestionIndex}/>
+
+
+  return (
+    <div>
+      <h1 className='d-flex justify-content-center'>Викторина</h1>
+      <QuestionButtons questions={[...questions]} answers={[...answers]} setQuestionIndex={setQuestionIndex} />
+      {questionIndex < questions.length 
+        ?
+        <Question arrProps={[questions, questionIndex, setAnswer, answer, submit]} />
+        :
         <div className='d-flex flex-column'>
           <h1 className='d-flex justify-content-center'>Финиш</h1>
-        <h2 className='d-flex justify-content-center'>Ваш процент успеха {Math.round(score/questions.length*100)}%</h2>
+          <h2 className='d-flex justify-content-center'>Ваш процент успеха {Math.round(score / questions.length * 100)}%</h2>
           <div className='d-flex justify-content-center my-4'>
             <button className='cstButton' type="button" onClick={restart}>
               повторить
             </button>
           </div>
         </div>
-        <p className='d-flex justify-content-center border border-dark py-2 bg-white'>счет: {score} / {questions.length}</p>
-      </div>
-    );
-  }
+      }
+      <p className='d-flex justify-content-center border border-dark py-2 bg-white'>счет: {score} / {questions.length}</p>
+    </div>
+  )
 }
--- src/QuestionButtons/QuestionButtons.js
+++ src/QuestionButtons/QuestionButtons.js
@@ -8,10 +8,14 @@ export default class QuestionButtons extends React.Component {
     }
 
     render() {
-            const questionList = [...this.props.questions].map((question, i) => {
+        const {questions, answers}= this.props;
+        const questionList = [...questions].map((question, i) => {
+            const liClass=["element"];
+            if(answers[i].value.length !== 0) {
+                answers[i].value? liClass.push("bg-success") : liClass.push("bg-danger")
+            }
             return (
-                // <li className='element' key={i} onClick={()=>this.props.setQuestionIndex(i)}> --- a feature that allows you to select a question number  
-                <li className='element' key={i}>
+                <li className={liClass.join(" ")} key={i} onClick={()=>this.props.setQuestionIndex(i)}>
                     {i+1}
                 </li>
             );
